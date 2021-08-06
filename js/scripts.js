8072e23099ba56a10855d0595a53c383c8a877c6
Afanasiev Sergey <masterchannelmusic@gmail.com>
2021-08-06 16:10:39 +0300

    added background for header on scroll and fixed bugs

--- js/scripts.js
+++ js/scripts.js
@@ -16,9 +16,27 @@ $(document).ready(function(){
             }
         }
     });
-    $('.header_burger').click(function(event){
+
+    $('.header_menu_new').click(function(event){
         $('.header_burger,.header_menu,.header_menu_new').toggleClass('active');
         $('body').toggleClass('lock');
+    });
+
+
+    function toggleScrollClass() {
+        let nav = document.querySelectorAll('.header')[0];
+        window.pageYOffset > 600? nav.classList.remove('fade')  : nav.classList.add('fade')
+    }
+    window.addEventListener('scroll', function() {toggleScrollClass()});
+    toggleScrollClass();
+
+
+
+    $('a[href^="#gallery_anchor"]').click(function(){ //
+        let anchor = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop:  $(anchor).offset().top
+        }, 600);
     });
 });
 
